8b5751d5f5e136bddb7de7c79c2bf64893f0d0be
Roshanpk619 <Roshanpk619@gmail.com>
2021-07-18 22:42:13 +0530

    undoing i "ResetStuff"
    This reverts commit 0ffed540db86bc9160df660bd0646464962eef2e.

--- src/test/java/com/smc/APITesting/testcases/TestCreateUser.java
+++ src/test/java/com/smc/APITesting/testcases/TestCreateUser.java
@@ -74,10 +74,4 @@ public class TestCreateUser extends Resources {
 		System.out.println("Test Project-Jakarta");
 
 	}
-	@Test(enabled = true)
-	public void SampleReset() {
-		 
-		System.out.println("SampleReset");
-
-	}
 }
